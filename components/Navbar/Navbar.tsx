478a491a79431f39d638d4223268be9529d04fa6
Witold <witekrychlik@o2.pl>
2024-02-21 19:54:43 +0100

    chor: facivon and logo added

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
 import { Wrapper, buttonVariants } from "../ui";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <div className="fixed top-0 inset-x-0 h-14 bg-primary">
       <header className="h-full">
         <Wrapper className="h-full flex flex-row justify-between items-center py-1">
-          <div className={buttonVariants({ variant: "secondary" })}>Logo</div>
+          <div className="ml-4 flex lg:ml-0">
+            <Link
+              href="/"
+              className="flex flex-row items-center gap-3 font-bold text-white"
+            >
+              <Image
+                src="/logo.svg"
+                alt="Znak firmowy Twoje Finanse "
+                width={40}
+                height={40}
+              />
+              <div>Twoje Finanse</div>
+            </Link>
+          </div>
+
           <nav className="flex flex-row items-center gap-3">
             <Link
               href="/#kontakt"
